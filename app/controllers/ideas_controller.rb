8e8a8c3b188f273727569e4b6f2e3af11fca3670
Jean-Baptiste `Kagnus` Brenaut <jean-baptiste.brenaut@epitech.eu>
2012-11-29 14:33:21 +0100

    Added delete, edit and changed view
    + added delete
    + added edit
    + added links to edit and delete in view

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,14 +1,41 @@
 class IdeasController < ApplicationController
   def create
-    @idea = Idea.new
+    if request.get?
+      @idea = Idea.new
+    elsif request.post?
+      @idea = Idea.new params[:idea]
+      if @idea.save
+        redirect_to :action => :view, :id => @idea
+      else
+        redirect_to :action => :edit, :id => @idea
+      end
+    end
   end
 
   def view
+    @idea = Idea.find(params[:id])
   end
 
   def delete
+    @idea = Idea.find(params[:id])
+    if request.post? and params[:confirm]
+      @idea.delete
+      redirect_to :controller => :boxes, :action => :view, :id => @idea.box
+    end
   end
 
   def edit
+    @idea = Idea.find(params[:id])
+    if request.post?
+
+      if params[:idea][:name] == ""
+        params[:idea].delete(:name)
+      end
+
+      if @idea.update_attributes(params[:idea])
+        redirect_to :action => :view, :id => @idea
+      end
+
+    end
   end
 end
