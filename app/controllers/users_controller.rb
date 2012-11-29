d2cb3a3b6212f29bf36a2b5b5ca7f974979bb56d
Adaedra <adaedra@gmail.com>
2012-11-29 14:39:51 +0100

    Added register form
    + Added register form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
   def register
+    if request.post?
+      @user = User.new(params[:user])
+      @user.role = User::User
+      if @user.save
+        redirect_to :action => :view, :id => @user
+      end
+    else
+      @user = User.new
+    end
   end
 
   def view
@@ -21,8 +30,15 @@ class UsersController < ApplicationController
   end
 
   def delete
+    @user = User.find(params[:id])
+
+    if params[:confirm]
+      @user.delete
+      redirect_to :action => :list
+    end
   end
 
   def list
+    @users = User.find(:all)
   end
 end
