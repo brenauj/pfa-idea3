1fe34482cb63e4259bc97f4c4c411aaa4331c801
Elodie Harkou <elodie.harkou@epitech.eu>
2012-12-13 17:37:11 +0100

    status added into ideas * status added in idea table * choice official or none added in create idea form for the status

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121120134515) do
+ActiveRecord::Schema.define(:version => 20121213155428) do
 
   create_table "boxes", :force => true do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20121120134515) do
     t.integer  "user_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "status"
   end
 
   add_index "ideas", ["box_id"], :name => "index_ideas_on_box_id"
