43a40671735d7daf897d8186b511af7ef87f1cb0
Jean-Baptiste `Kagnus` Brenaut <jean-baptiste.brenaut@epitech.eu>
2012-12-13 11:09:23 +0100

    Added access to box_id in Idea model
    + access to box_id in Idea

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,7 +2,7 @@ class Idea < ActiveRecord::Base
   belongs_to :box
   belongs_to :user
 
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :box_id
 
   validates_presence_of :name, :description
   validates_length_of :name, :minimum => 6
